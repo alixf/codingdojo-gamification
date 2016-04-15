f38cdfaedf0ca2f782127a401837267e32f83b77
Alix Fumoleau <alix.fumoleau@gmail.com>
2016-04-15 07:54:34 +0200

    Remove Java project

--- codingdojo-exercices/src/main/java/com/betomorow/codingdojos/exercices/Settings.java
+++
@@ -1,7 +0,0 @@
-package com.betomorow.codingdojos.exercices;
-
-public class Settings {
-    public static final String USERNAME = "Fixme";
-    public static final String POST_URL = "http://localhost/post";
-    public static final String CODE_URL = "http://localhost/code";
-}
